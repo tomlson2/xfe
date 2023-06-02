05397251fed45ae1f6f9160764aa65e4d7badcd9
Thomas Bryson <thomasmbryson@gmail.com>
2023-06-02 00:40:00 -0700

    small update for live site

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -16,11 +16,8 @@ function App() {
                     <div className="yellowSubtext">welcome to x... proof you were early</div>
                 </div>
                 <div className="linksWrapper">
-                    <a href="https://unisat.io/brc20/%F0%90%8D%87" className="link">10</a>
-                    <a href="https://unisat.io/brc20/%F0%90%8A%B4" className="link">100</a>
-                    <a href="https://unisat.io/brc20/%F0%9D%92%B3" className="link">1,000</a>
-                    <a href="https://unisat.io/brc20/%F0%90%A4%95" className="link">10,000</a>
-                    <a href="https://unisat.io/brc20/%F0%9F%97%B4" className="link">100,000</a>
+                    <a href="https://unisat.io/brc20/XMYR" className="link">10,000 Collection</a>
+                    <a href="https://unisat.io/brc20/%F0%9F%97%B4" className="link">100,000 Collection</a>
                 </div>
             </div>
         </div>
